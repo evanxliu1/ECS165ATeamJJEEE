6b8e57c44a6fd1a9c348e989aa291cb4de327bef
josephzlay1 <josephzlay@gmail.com>
2026-02-05 20:52:37 -0800

    Create joseph.py

---
+++ joseph.py
@@ -0,0 +1,47 @@
+"""
+A data strucutre holding indices for various columns of a table. Key column should be indexd by default, other columns can be indexed through this object. Indices are usually B-Trees, but other data structures can be used as well.
+"""
+
+class Index:
+
+    def __init__(self, table):
+        # One index for each table. All our empty initially.
+        self.indices = [None] *  table.num_columns
+        pass
+
+    """
+    # returns the location of all records with the given value on column "column"
+    """
+
+    def locate(self, column, value):
+        all = self.indices[column]
+        if all.has_key[value]:
+            return all.[value]
+        else:
+            return None
+
+    """
+    # Returns the RIDs of all records with values in column "column" between "begin" and "end"
+    """
+
+    def locate_range(self, begin, end, column):
+        if begin > end:
+            return (self.all[column].values(min = end, max = begin))[::-1]
+        elif end > begin:
+            return (self.all[column].values(min = begin, max = end))
+        else:
+            return None
+
+    """
+    # optional: Create index on specific column
+    """
+
+    def create_index(self, column_number):
+        pass
+
+    """
+    # optional: Drop index of specific column
+    """
+
+    def drop_index(self, column_number):
+        pass
