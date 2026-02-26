5a371bb920b94c8c0b58aecafe31261d2787e6db
josephzlay1 <josephzlay@gmail.com>
2026-02-26 01:19:52 -0800

    Redo table.py comments

--- lstore/table.py
+++ lstore/table.py
@@ -3,15 +3,24 @@ from lstore.index import Index
 from lstore.config import *
 
 class Record:
+    """
+    #Creates Record object
+    """
     def __init__(self, rid, key, columns):
         self.rid = rid
         self.key = key
         self.columns = columns
 
+    """
+    #Creates __getitem__ method to allow objects of class Record to be accessed with an index
+    """
     def __getitem__(self, idx):
         return self.columns[idx]
 
 class PageRange:
+    """
+    #Creates PageRange object
+    """
     def __init__(self, num_cols, table_name=None, range_idx=0, bufferpool=None):
         self.num_cols = num_cols
         self.table_name = table_name
@@ -21,12 +30,28 @@ class PageRange:
         self.num_tail_records = 0
         self.tps = {}
 
+    """
+    #Creates a unique identifier (ID) for a specific page
+    :param is_tail: boolean     #returns true or false depending on whether the current page is a tail page or base page
+    :param page_idx: int     #which page inside the page range
+    :param col_idx: int     #which column this page stores
+    """
     def _page_id(self, is_tail, page_idx, col_idx):
         return (self.table_name, self.range_idx, is_tail, page_idx, col_idx)
 
+
+    """
+    #Returns T/F based on whether the page range still has room for more base records
+    no param
+    """
     def has_capacity(self):
         return self.num_base_records < RECORDS_PER_PAGE_RANGE
 
+
+    """
+    #Inserts one base record into storage using the buffer pool
+    :param vals: list     #corresponds to column's value
+    """
     def add_base_record(self, vals):
         pgnum = self.num_base_records // RECORDS_PER_PAGE
         sl = self.num_base_records % RECORDS_PER_PAGE
@@ -41,6 +66,11 @@ class PageRange:
         self.num_base_records = self.num_base_records + 1
         return pgnum, sl
 
+
+    """
+    #Same thing as the function above but adds one tail record instead
+    :param vals: list     #column's value
+    """
     def add_tail_record(self, vals):
         pgnum = self.num_tail_records // RECORDS_PER_PAGE
         sl = self.num_tail_records % RECORDS_PER_PAGE
@@ -55,14 +85,36 @@ class PageRange:
         self.num_tail_records = self.num_tail_records + 1
         return pgnum, sl
 
+
+    """
+    #Returns the value stored in base page, column, at slot using the buffer pool
+    :param pg: int     #which base page
+    :param slot: int     #which row position within that page
+    :param col: int     #which column
+    """
     def get_base_val(self, pg, slot, col):
         page_id = self._page_id(False, pg, col)
         return self.bufferpool.read_value(page_id, slot)
 
+
+    """
+    #Returns the value stored in tail page, column, at slot
+    :param pg: int     #which base page
+    :param slot: int     #which row position
+    :param col: int     #which column
+    """
     def get_tail_val(self, pg, slot, col):
         page_id = self._page_id(True, pg, col)
         return self.bufferpool.read_value(page_id, slot)
 
+
+    """
+    #Reads num_cols consecutive columns from a base record located at pg, slot and returns as list
+    :param pg: int     #which base page
+    :param slot: int     #which record position within that page
+    :param start_col: int     #starting column index
+    :param num_cols: int     #how many columns to read
+    """
     def get_base_vals(self, pg, slot, start_col, num_cols):
         vlist = []
         for i in range(num_cols):
@@ -70,6 +122,14 @@ class PageRange:
             vlist.append(self.bufferpool.read_value(page_id, slot))
         return vlist
 
+
+     """
+    #Reads num_cols consecutive columns from a tail record located at pg, slot and returns as list
+    :param pg: int     #which base page
+    :param slot: int     #which record position within that page
+    :param start_col: int     #starting column index
+    :param num_cols: int     #how many columns to read
+    """
     def get_tail_vals(self, pg, slot, start_col, num_cols):
         vlist = []
         for i in range(num_cols):
@@ -77,6 +137,14 @@ class PageRange:
             vlist.append(self.bufferpool.read_value(page_id, slot))
         return vlist
 
+
+    """
+    #Writes val into base page pg, column col, at position slot
+    :param pg: int     #which base page
+    :param slot: int     #which record position within the page
+    :param col: int     #which column
+    :param val: any (depending on the column)     #the value being inserted
+    """
     def set_base_val(self, pg, slot, col, val):
         page_id = self._page_id(False, pg, col)
         page_obj = self.bufferpool.get_page(page_id)
@@ -85,6 +153,9 @@ class PageRange:
         self.bufferpool.unpin(page_id)
 
 class Table:
+    """
+    #Creates Table object
+    """
     def __init__(self, name, num_columns, key, bufferpool=None):
         self.name = name
         self.key = key
@@ -97,11 +168,17 @@ class Table:
         self.merge_thread = None
         self.index = Index(self)
 
+    """
+    #Generates a new unique record ID every time this method is called
+    """
     def new_rid(self):
         r = self.next_rid
         self.next_rid = self.next_rid + 1
         return r
 
+    """
+    #Decides which page range new records should go into
+    """
     def _current_range(self):
         if len(self.page_ranges) == 0:
             self.page_ranges.append(PageRange(self.total_cols, table_name=self.name, range_idx=0, bufferpool=self.bufferpool))
@@ -113,7 +190,11 @@ class Table:
             return rng_ix, self.page_ranges[-1]
         return len(self.page_ranges) - 1, last
 
-    # takes updates from tail pages and merges latest back inot base
+    """
+    #Takes updates stored in tail pages and applies the latest values back into base pages
+    :param range_idx: 
+    def merge(self, range_idx): int     #index of a pagerange inside self.page_ranges
+    """
     def merge(self, range_idx):
         try:
             prange = self.page_ranges[range_idx]
@@ -146,6 +227,10 @@ class Table:
         except Exception:
             pass
 
+    """
+    #Decides whether or not to start a background merge
+    :param range_idx: int     #index of a page range
+    """
     def maybe_trigger_merge(self, range_idx):
         prange = self.page_ranges[range_idx]
         if prange.num_tail_records < MERGE_THRESHOLD:
